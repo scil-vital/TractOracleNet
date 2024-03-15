2426a4cf060858f93fbeefae9fd298e4a2eeaad4
AntoineTheb <antoine.theberge@usherbrooke.ca>
2024-03-15 13:25:03 +0000

    FIX: Mac fix

--- install.sh
+++ install.sh
@@ -14,17 +14,18 @@ if [ -x "$(command -v nvidia-smi)" ]; then
       echo "No suitable CUDA version found. Installing PyTorch without CUDA support"
     fi
 else
-    # if on MAC
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        CUDA_VERSION=""
-    else
-        CUDA_VERSION="cpu"
-    fi
+    CUDA_VERSION="cpu"
 fi
-# Install pytorch
-echo "Installing Pytorch 1.13.1+${CUDA_VERSION}"
-# Install PyTorch with CUDA support
-pip install torch==1.13.1+${CUDA_VERSION} torchvision==0.14.1+${CUDA_VERSION} torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION} --quiet
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    pip install torch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 --quiet
+else
+    # Install pytorch
+    echo "Installing Pytorch 1.13.1+${CUDA_VERSION}"
+    # Install PyTorch with CUDA support
+    pip install torch==1.13.1+${CUDA_VERSION} torchvision==0.14.1+${CUDA_VERSION} torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION} --quiet
+fi
+
 # Install other required packages and modules
 echo "Installing final required packages"
 pip install -e . --quiet
