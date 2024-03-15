dcd8829a748c1aed855d0b595a561a684ef417b6
AntoineTheb <antoine.theberge@usherbrooke.ca>
2024-03-15 13:45:56 +0000

    ENH: install wording

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 # Install required packages
 # Print python version
 echo "Python version: $(python --version)"
-echo "Installing required packages"
-pip install Cython numpy packaging --quiet
 # If platform has CUDA installed
 if [ -x "$(command -v nvidia-smi)" ]; then
     # Print GPU name
@@ -17,24 +15,28 @@ if [ -x "$(command -v nvidia-smi)" ]; then
         CUDA_VERSION="cu117"
     else
       CUDA_VERSION="cpu"
-      echo "No suitable CUDA version found. Installing PyTorch without CUDA support"
+      echo "No suitable CUDA version found. Installing PyTorch without CUDA support."
     fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Platform: macOS"
 else
-    echo "No GPU or CUDA installation found. Installing PyTorch without CUDA support"
+    echo "No GPU or CUDA installation found. Installing PyTorch without CUDA support."
     CUDA_VERSION="cpu"
 fi
 
+echo "Installing required packages."
+pip install Cython numpy packaging --quiet
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Platform: macOS"
-    echo "Installing Pytorch 1.13.1"
+    echo "Installing PyTorch 1.13.1"
     pip install torch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 --quiet
 else
     # Install pytorch
-    echo "Installing Pytorch 1.13.1+${CUDA_VERSION}"
+    echo "Installing PyTorch 1.13.1+${CUDA_VERSION}"
     # Install PyTorch with CUDA support
     pip install torch==1.13.1+${CUDA_VERSION} torchvision==0.14.1+${CUDA_VERSION} torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/${CUDA_VERSION} --quiet
 fi
 
 # Install other required packages and modules
-echo "Installing rest of packages"
+echo "Finalizing installation..."
 pip install -e . --quiet
+echo "Done !"
